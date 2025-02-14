0c3be2c2c10a6d589ff69de2e57f3e39e2cead1c
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-02-14 23:16:43 +0100

    Disable file remove and file rename implementation, underlying function does not exists (also we don't need it anyway)

--- src/z8lua/loslib.c
+++ src/z8lua/loslib.c
@@ -89,17 +89,17 @@ static int os_execute (lua_State *L) {
 }
 
 
-static int os_remove (lua_State *L) {
-  const char *filename = luaL_checkstring(L, 1);
-  return luaL_fileresult(L, remove(filename) == 0, filename);
-}
+//static int os_remove (lua_State *L) {
+//  const char *filename = luaL_checkstring(L, 1);
+//  return luaL_fileresult(L, remove(filename) == 0, filename);
+//}
 
 
-static int os_rename (lua_State *L) {
-  const char *fromname = luaL_checkstring(L, 1);
-  const char *toname = luaL_checkstring(L, 2);
-  return luaL_fileresult(L, rename(fromname, toname) == 0, NULL);
-}
+//static int os_rename (lua_State *L) {
+//  const char *fromname = luaL_checkstring(L, 1);
+//  const char *toname = luaL_checkstring(L, 2);
+//  return luaL_fileresult(L, rename(fromname, toname) == 0, NULL);
+//}
 
 
 static int os_tmpname (lua_State *L) {
@@ -304,8 +304,8 @@ static const luaL_Reg syslib[] = {
   {"execute",   os_execute},
   {"exit",      os_exit},
   {"getenv",    os_getenv},
-  {"remove",    os_remove},
-  {"rename",    os_rename},
+//  {"remove",    os_remove},
+//  {"rename",    os_rename},
   {"setlocale", os_setlocale},
   {"time",      os_time},
   {"tmpname",   os_tmpname},
