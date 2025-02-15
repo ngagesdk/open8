ede5777fdd70e567efb7fe1fdcc9d62fcb65f0d7
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-02-15 22:11:26 +0100

    Redirect Lua output to SDL_Log()

--- src/api.c
+++ src/api.c
@@ -11,13 +11,39 @@
 #include "z8lua/lauxlib.h"
 #include "z8lua/lua.h"
 
-int pico8_pset(lua_State* L)
-{
-    return 0;
-}
+static int pico8_pset(lua_State* L);
+static int pico8_SDL_log(lua_State* L);
 
 void register_api(lua_State* L)
 {
     lua_pushcfunction(L, pico8_pset);
     lua_setglobal(L, "pset");
+
+    lua_pushcfunction(L, pico8_SDL_log);
+    lua_setglobal(L, "SDL_log");
+}
+
+static int pico8_pset(lua_State* L)
+{
+    return 0;
+}
+
+static int pico8_SDL_log(lua_State* L)
+{
+    int n = lua_gettop(L);
+    lua_getglobal(L, "tostring");
+    for (int i = 1; i <= n; i++)
+    {
+        lua_pushvalue(L, -1);
+        lua_pushvalue(L, i);
+        lua_call(L, 1, 1);
+        const char* s = lua_tostring(L, -1);
+        if (s == NULL)
+        {
+            return luaL_error(L, "'tostring' must return a string to 'print'");
+        }
+        SDL_Log("%s", s);
+        lua_pop(L, 1);
+    }
+    return 0;
 }
