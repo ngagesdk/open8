bcb1bf548d4aa105b18ab73568edeb80a6bdea4f
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-02-28 19:46:37 +0100

    Fix #24, Kudos to @nightmareci

--- src/z8lua/lparser.c
+++ src/z8lua/lparser.c
@@ -1382,8 +1382,8 @@ static void fornum (LexState *ls, TString *varname, int line) {
   exp1(ls);  /* limit */
   if (testnext(ls, ','))
     exp1(ls);  /* optional step */
-  else {  /* default step = 1 */
-    luaK_codek(fs, fs->freereg, luaK_numberK(fs, 1));
+  else {  /* default step = 1.0 in 16:16 fixed-point */
+    luaK_codek(fs, fs->freereg, luaK_numberK(fs, fix32_value(1, 0)));
     luaK_reserveregs(fs, 1);
   }
   forbody(ls, base, line, 1, 1);
