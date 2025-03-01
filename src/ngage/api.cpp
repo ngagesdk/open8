48e4b61386af765afb311fc55cfc709dbcae9595
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-03-01 09:40:11 +0100

    Rename/rebrand emulator to open8, minor re-organisation

--- src/ngage/api.cpp
+++ src/ngage/api.cpp
@@ -1,6 +1,6 @@
 /** @file api_ngage.cpp
  *
- *  A Pico-8 emulator for the Nokia N-Gage.
+ *  A portable PICO-8 emulator written in C.
  *
  *  Copyright (c) 2025, Michael Fitzmayer. All rights reserved.
  *  SPDX-License-Identifier: MIT
