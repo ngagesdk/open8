903e0ab905f0082737254f6a6d862969e4ebaa02
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-03-10 12:27:23 +0100

    Update tests, add more stub functions, use latest SDL3 release version.

--- tests/tests.c
+++ tests/tests.c
@@ -18,7 +18,7 @@ int main()
     luaL_openlibs(vm);
     init_api(vm);
 
-    if (luaL_loadfile(vm, "tests.p8") || lua_pcall(vm, 0, 1, 0))
+    if (luaL_loadfile(vm, "tests.lua") || lua_pcall(vm, 0, 1, 0))
     {
         SDL_Log("Lua error: %s", lua_tostring(vm, -1));
         lua_pop(vm, 1);
