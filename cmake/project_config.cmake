35304aeac9cb2f60ca1b64902bb2f55d197ecbec
Cameron Cawley <ccawley2011@gmail.com>
2025-05-05 21:19:51 +0100

    Use the full frame from the game data file

--- cmake/project_config.cmake
+++ cmake/project_config.cmake
@@ -6,7 +6,6 @@ set(project_sources
   ${CMAKE_CURRENT_SOURCE_DIR}/src/app.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/auxiliary.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/core.c
-  ${CMAKE_CURRENT_SOURCE_DIR}/src/image_loader.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/memory.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/p8scii.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/lexaloffle/p8_compress.c
