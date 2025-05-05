82d1054257c3408cd1937a53e354a6d8474f25c8
Cameron Cawley <ccawley2011@gmail.com>
2025-05-05 21:19:51 +0100

    Use the full frame from the game data file

--- src/config.h
+++ src/config.h
@@ -12,35 +12,35 @@
 
 #include "SDL3/SDL.h"
 
-#define SCREEN_SIZE 128
 
 #ifdef __SYMBIAN32__
 #define SCALE 1
 #define WINDOW_W 176
 #define WINDOW_H 208
 #define WINDOW_FLAGS 0
-#define FRAME_OFFSET_X 0
-#define FRAME_OFFSET_Y 0
-#define SCREEN_OFFSET_X 24
-#define SCREEN_OFFSET_Y 25
+#define FRAME_OFFSET_X 8
+#define FRAME_OFFSET_Y 1
 #elif defined __3DS__
 #define SCALE 1
 #define WINDOW_W 400
 #define WINDOW_H 240
 #define WINDOW_FLAGS 0
-#define FRAME_OFFSET_X 112
-#define FRAME_OFFSET_Y 16
-#define SCREEN_OFFSET_X FRAME_OFFSET_X + 24
-#define SCREEN_OFFSET_Y FRAME_OFFSET_Y + 25
+#define FRAME_OFFSET_X 120
+#define FRAME_OFFSET_Y 17
 #else
 #define SCALE 3
 #define WINDOW_W 176
 #define WINDOW_H 208
 #define WINDOW_FLAGS (SDL_WINDOW_UTILITY)
-#define FRAME_OFFSET_X 0
-#define FRAME_OFFSET_Y 0
-#define SCREEN_OFFSET_X 24
-#define SCREEN_OFFSET_Y 25
+#define FRAME_OFFSET_X 8
+#define FRAME_OFFSET_Y 1
 #endif
+
+#define FRAME_W 160
+#define FRAME_H 205
+
+#define SCREEN_OFFSET_X FRAME_OFFSET_X + 16
+#define SCREEN_OFFSET_Y FRAME_OFFSET_Y + 24
+#define SCREEN_SIZE 128
 
 #endif // CONFIG_H
