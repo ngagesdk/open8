e0af5cde86b75b4f68e856645aee41a56774b3b8
Cameron Cawley <ccawley2011@gmail.com>
2025-05-09 21:45:21 +0100

    Replace use of clock() with SDL_GetTicks()

--- cmake/project_config.cmake
+++ cmake/project_config.cmake
@@ -11,9 +11,6 @@ set(project_sources
   ${CMAKE_CURRENT_SOURCE_DIR}/src/lexaloffle/p8_compress.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/lexaloffle/pxa_compress_snippets.c)
 
-set(ngage_sources
-  ${CMAKE_CURRENT_SOURCE_DIR}/src/ngage/api.cpp)
-
 set(UID3 0x1000c37e) # game.exe UID
 set(APP_UID 0x100051C0) # open8.app UID
 set(APP_NAME "open8")
