5dedbe64bd563587378fccccccb389fbbd548bc3
Cameron Cawley <ccawley2011@gmail.com>
2025-05-09 21:30:06 +0100

    Use the gamepad API instead of the joystick API

--- src/app.c
+++ src/app.c
@@ -23,11 +23,16 @@ bool init_app(SDL_Renderer** renderer, SDL_Window* window)
 #ifdef __SYMBIAN32__
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
 #else
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK))
+    if (!SDL_Init(SDL_INIT_VIDEO))
 #endif
     {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return false;
+    }
+
+    if (!SDL_InitSubSystem(SDL_INIT_GAMEPAD))
+    {
+        SDL_Log("Couldn't initialize gamepad subsystem: %s", SDL_GetError());
     }
 
     window = SDL_CreateWindow("open8", WINDOW_W * SCALE, WINDOW_H * SCALE, WINDOW_FLAGS);
